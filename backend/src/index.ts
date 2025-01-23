43c710f1d8ef58e6a0ea279b38bcff0b1717be1f
Efemiaya Favour Tega <efemiayafavour@gmail.com>
2025-01-23 16:17:59 +0100

    done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,12 @@ import cookieParser from "cookie-parser"
 
 import dotenv from "dotenv"
 import { app, server } from "./socket/socket.js";
+import { run } from "node:test";
+
 dotenv.config();
 
+
 const PORT = process.env.PORT || 5000
-
-
 
 app.use(express.json());
 app.use(cookieParser());
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -2,7 +2,11 @@ import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
 import { sendMessage, getMessage, getUserForsidebar } from "../controllers/message.controller.js";
 
+
+
+
 const router = express.Router()
+
 
 
 router.get('/conversations', protectRoute, getUserForsidebar);
